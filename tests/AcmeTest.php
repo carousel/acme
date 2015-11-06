1cae529ef46e0cdf8a55535e181008246630b9e8
Miroslav Trninic <miroslav.trninic@gmail.com>
2015-11-06 21:00:55 +0100

    adding initial unit test

--- tests/AcmeTest.php
+++ tests/AcmeTest.php
@@ -1,10 +1,15 @@
 <?php
 
-class AcmeTest extends \PHPUnit_Framework_TestCase 
+require __DIR__ . '../../vendor/autoload.php';
+
+use Acme\Code\User;
+
+class AcmeTest extends PHPUnit_Framework_TestCase 
 {
     /** @test */
-    public function acme()
+    public function acmeUserTest()
     {
-        $this->assertTrue(true);
+        $user = new User('Miro');
+        $this->assertEquals($user->name,'Miro');
     }
 }
